88ca9f7dcdc4388ed1c0a5a14dbf06e51a0f8cd7
Alex Cameron <asc@tetsuo.sh>
2022-03-23 01:09:08 +1100

    fulcio: Add client interface

--- sigstore/fulcio/_client.py
+++ sigstore/fulcio/_client.py
@@ -2,6 +2,22 @@
 Client implementation for interacting with Fulcio.
 """
 
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class CertificateRequest:
+    pass
+
+
+@dataclass(frozen=True)
+class CertificateResponse:
+    pass
+
 
 class FulcioClient:
-    pass
+    def __init__(self, url: str) -> None:
+        self.url = url
+
+    def signing_cert(self, req: CertificateRequest) -> CertificateResponse:
+        raise NotImplementedError
