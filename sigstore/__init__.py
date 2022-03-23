53143402671f0d3bfde10445ae54becb93d3f513
Dustin Ingram <di@users.noreply.github.com>
2022-03-23 22:19:32 +0000

    Add arguments to sign/verify commands

--- sigstore/__init__.py
+++ sigstore/__init__.py
@@ -2,16 +2,8 @@
 The `sigstore` APIs.
 """
 
+from sigstore._sign import sign
+from sigstore._verify import verify
 from sigstore._version import __version__
 
-__all__ = ["__version__"]
-
-
-def sign():
-    """Public API for signing blobs"""
-    return "Nothing here yet"
-
-
-def verify():
-    """Public API for verifying blob signatures"""
-    return "Nothing here yet"
+__all__ = ["__version__", "sign", "verify"]
--- sigstore/_cli.py
+++ sigstore/_cli.py
@@ -9,10 +9,21 @@ def main():
 
 
 @main.command("sign")
-def _sign():
-    click.echo(sign())
+@click.option("identity_token", "--identity-token", type=click.STRING)
+@click.argument("file_", metavar="FILE", type=click.File("r"), required=True)
+def _sign(file_, identity_token):
+    click.echo(sign(file_=file_, identity_token=identity_token, output=click.echo))
 
 
 @main.command("verify")
-def _verify():
-    click.echo(verify())
+@click.option("certificate_path", "--cert", type=click.Path())
+@click.option("signature_path", "--signature", type=click.Path())
+@click.argument("file_", metavar="FILE", type=click.File("r"), required=True)
+def _verify(file_, certificate_path, signature_path):
+    click.echo(
+        verify(
+            filename=file_.name,
+            certificate_path=certificate_path,
+            signature_path=signature_path,
+        )
+    )
