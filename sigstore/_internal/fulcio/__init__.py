6d8ad84f13a5d8644e04c0f7c95a64d356cc3bc0
Alex Cameron <asc@tetsuo.sh>
2022-04-11 00:27:08 +1000

    sct: Add SCT verification

--- sigstore/_internal/fulcio/__init__.py
+++ sigstore/_internal/fulcio/__init__.py
@@ -7,10 +7,12 @@ from ._client import (
     FulcioCertificateSigningRequest,
     FulcioCertificateSigningResponse,
     FulcioClient,
+    FulcioSignedCertificateTimestamp,
 )
 
 __all__ = [
     "FulcioCertificateSigningRequest",
     "FulcioCertificateSigningResponse",
     "FulcioClient",
+    "FulcioSignedCertificateTimestamp",
 ]
