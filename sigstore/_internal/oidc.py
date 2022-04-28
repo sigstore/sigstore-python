3929f790b10079d123f21cad01ae980ff85f48fc
Luke Hinds <7058938+lukehinds@users.noreply.github.com>
2022-04-28 14:51:22 +0100

    Migrate repo fields / governance to sigstore (#45)
    Migrate repo fields / governance to sigstore
    
    Signed-off-by: Luke Hinds <lhinds@redhat.com>

--- sigstore/_internal/oidc.py
+++ sigstore/_internal/oidc.py
@@ -1,3 +1,17 @@
+# Copyright 2022 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import jwt
 
 # From https://github.com/sigstore/fulcio/blob/b2186c01da1ddf807bde3ea8c450226d8e001d88/pkg/config/config.go#L182-L201  # noqa
