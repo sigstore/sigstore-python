2178a1c866984708683857a2c4e3ccb9d419ff64
William Woodruff <william@trailofbits.com>
2022-05-05 00:44:02 -0600

    build: move almost everything to pyproject.toml (#70)
    * build: move almost everything to pyproject.toml
    
    This requires a `MANIFEST.in`, since `setuptools` apparently
    can't specify package data in `pyproject.toml`. Yet.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * pyproject: remove MANIFEST.in, embed package-data
    
    I was wrong!
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * workflows/ci: remove reference to pip-audit
    
    sigstore uses click, so this isn't relevant.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * setup.py: Fix typo
    
    Signed-off-by: Alex Cameron <asc@tetsuo.sh>
    
    Co-authored-by: Alex Cameron <asc@tetsuo.sh>

--- setup.py
+++ setup.py
@@ -13,76 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib
+"""Shim for editable installs."""
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-version = importlib.import_module("sigstore._version")
-
-with open("./README.md") as f:
-    long_description = f.read()
-
-setup(
-    name="sigstore",
-    version=version.__version__,
-    license="Apache-2.0",
-    author="Sigstore Authors",
-    author_email="sigstore-dev@googlegroups.com",
-    description="A tool for signing Python package distributions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sigstore/sigstore-python",
-    packages=find_packages(),
-    package_data={"sigstore": ["_store/*"]},
-    include_package_data=True,
-    entry_points={
-        "console_scripts": [
-            "sigstore = sigstore._cli:main",
-        ]
-    },
-    platforms="any",
-    python_requires=">=3.7",
-    install_requires=[
-        "click>=8",
-        "cryptography",
-        "pem",
-        "pydantic",
-        "pyjwt",
-        "pyOpenSSL",
-        "requests",
-        "securesystemslib",
-    ],
-    extras_require={
-        "dev": [
-            "build",
-            "bump",
-            "flake8",
-            "black",
-            "isort",
-            "pytest",
-            "pytest-cov",
-            "pretend",
-            "coverage[toml]",
-            "interrogate",
-            "pdoc3",
-            "mypy",
-            "types-cryptography",
-            "types-requests",
-            "types-pyOpenSSL",
-            "types-pyjwt",
-        ]
-    },
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Security",
-        "Topic :: Security :: Cryptography",
-    ],
-)
+setup()
