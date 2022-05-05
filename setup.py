2dfd0fd89235b3a461e119c816377d60c3583b62
Alex Cameron <asc@tetsuo.sh>
2022-05-05 16:40:32 +1000

    setup.py: Fix typo
    Signed-off-by: Alex Cameron <asc@tetsuo.sh>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Shim for editble installs."""
+"""Shim for editable installs."""
 from setuptools import setup
 
 setup()
