84f74ebc177314e416e18ec208834ef0394588e3
Alex Cameron <asc@tetsuo.sh>
2022-05-10 22:42:12 +1000

    fulcio, _sign: Request certificates via CSR
    Signed-off-by: Alex Cameron <asc@tetsuo.sh>

--- sigstore/_internal/fulcio/__init__.py
+++ sigstore/_internal/fulcio/__init__.py
@@ -18,14 +18,12 @@ APIs for interacting with Fulcio.
 
 
 from ._client import (
-    FulcioCertificateSigningRequest,
     FulcioCertificateSigningResponse,
     FulcioClient,
     FulcioSignedCertificateTimestamp,
 )
 
 __all__ = [
-    "FulcioCertificateSigningRequest",
     "FulcioCertificateSigningResponse",
     "FulcioClient",
     "FulcioSignedCertificateTimestamp",
