d271a74c481d5405cf1497f63973bd208b69b592
William Woodruff <william@trailofbits.com>
2022-05-17 16:56:47 -0400

    sigstore, test: more unit tests, rename mods
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- test/internal/rekor/__init__.py
+++ test/internal/rekor/__init__.py
@@ -11,22 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-APIs for interacting with Fulcio.
-"""
-
-
-from .client import (
-    FulcioCertificateSigningRequest,
-    FulcioCertificateSigningResponse,
-    FulcioClient,
-    FulcioSignedCertificateTimestamp,
-)
-
-__all__ = [
-    "FulcioCertificateSigningRequest",
-    "FulcioCertificateSigningResponse",
-    "FulcioClient",
-    "FulcioSignedCertificateTimestamp",
-]
