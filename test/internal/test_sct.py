62941eaa37c80fbc1a4f57f7fbb60629988d1738
William Woodruff <william@trailofbits.com>
2022-05-26 12:29:08 -0400

    test/internal: add a test for _pack_digitally_signed
    Signed-off-by: William Woodruff <william@trailofbits.com>

---
+++ test/internal/test_sct.py
@@ -0,0 +1,58 @@
+# Copyright 2022 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import struct
+
+import pretend
+import pytest
+
+from sigstore._internal import sct
+
+
+@pytest.mark.parametrize(
+    "precert_bytes",
+    [
+        b"tbs",
+        b"x" * 255,
+        b"x" * 1024,
+        b"x" * 16777215,
+    ],
+)
+def test_pack_digitally_signed(precert_bytes):
+    mock_sct = pretend.stub(
+        version=pretend.stub(value=0),
+        timestamp=datetime.datetime.fromtimestamp(0x00000000000004D2 / 1000.0),
+        entry_type=pretend.stub(value=1),
+        extension_bytes=b"",
+    )
+    cert = pretend.stub(tbs_precertificate_bytes=precert_bytes)
+    issuer_key_hash = b"iamapublickeyshatwofivesixdigest"
+
+    _, l1, l2, l3 = struct.unpack("!4c", struct.pack("!I", len(precert_bytes)))
+
+    data = sct._pack_digitally_signed(mock_sct, cert, issuer_key_hash)
+    assert data == (
+        b"\x00"  # version
+        b"\x00"  # signature type
+        b"\x00\x00\x00\x00\x00\x00\x04\xd2"  # timestamp
+        b"\x00\x01"  # entry type
+        b"iamapublickeyshatwofivesixdigest"  # issuer key hash
+        + l1
+        + l2
+        + l3  # tbs cert length
+        + precert_bytes  # tbs cert
+        + b"\x00\x00"  # extensions length
+        + b""  # extensions
+    )
