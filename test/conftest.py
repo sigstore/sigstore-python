cd9f508da0f3f35a30c67115b1a36adf33757530
William Woodruff <william@trailofbits.com>
2022-08-01 15:53:56 -0400

    test: add an "online" marker for online tests
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- test/conftest.py
+++ test/conftest.py
@@ -21,6 +21,21 @@ _ASSETS = (Path(__file__).parent / "assets").resolve()
 assert _ASSETS.is_dir()
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--skip-online", action="store_true", help="skip tests that require network connectivity"
+    )
+
+
+def pytest_runtest_setup(item):
+    if "online" in item.keywords and item.config.getoption("--skip-online"):
+        pytest.skip("skipping test that requires network connectivity due to `--skip-online` flag")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "online: mark test as requiring network connectivity")
+
+
 @pytest.fixture
 def signed_asset():
     def _signed_asset(name: str) -> Tuple[bytes, bytes, bytes]:
