3fc02d8bde82a4702bf34993baabb64d43bcc1d4
William Woodruff <william@trailofbits.com>
2022-10-05 14:17:20 -0400

    test: add a signing test for log entry consistency
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- test/test_sign.py
+++ test/test_sign.py
@@ -12,7 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import secrets
 
+import pytest
+
+from sigstore._internal.oidc.ambient import detect_credential
 from sigstore._sign import Signer
 
 
@@ -24,3 +28,18 @@ def test_signer_production():
 def test_signer_staging():
     signer = Signer.staging()
     assert signer is not None
+
+
+@pytest.mark.parametrize("signer", [Signer.production(), Signer.staging()])
+def test_sign_rekor_entry_consistent(signer):
+    token = detect_credential()
+    if token is None:
+        pytest.skip("no ambient credentials; skipping")
+
+    payload = secrets.token_bytes(32)
+    result = signer.sign(payload, token)
+
+    expected_entry = result.log_entry
+    actual_entry = signer._rekor.log.entries.get(log_index=expected_entry.log_index)
+
+    assert expected_entry == actual_entry
