51b5009cc37641124af447f35715e7251d2ade90
William Woodruff <william@trailofbits.com>
2023-05-02 09:57:14 -0400

    oidc: fixup merge
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- sigstore/oidc.py
+++ sigstore/oidc.py
@@ -31,7 +31,6 @@ import jwt
 import requests
 from pydantic import BaseModel, StrictStr
 
-from sigstore._internal.oidc import DEFAULT_AUDIENCE
 from sigstore.errors import Error, NetworkError
 
 DEFAULT_OAUTH_ISSUER_URL = "https://oauth2.sigstore.dev/auth"
@@ -324,6 +323,6 @@ class IdentityError(Error):
 def detect_credential() -> Optional[str]:
     """Calls `id.detect_credential`, but wraps exceptions with our own exception type."""
     try:
-        return cast(Optional[str], id.detect_credential(DEFAULT_AUDIENCE))
+        return cast(Optional[str], id.detect_credential(_DEFAULT_AUDIENCE))
     except id.IdentityError as exc:
         IdentityError.raise_from_id(exc)
