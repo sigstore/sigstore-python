f7aef97be3e0a4935c4d84224e86d5bd1a33d487
William Woodruff <william@trailofbits.com>
2023-10-04 14:45:36 -0400

    sign: another typo
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- sigstore/sign.py
+++ sigstore/sign.py
@@ -217,7 +217,9 @@ class Signer:
             spec=sigstore_rekor_types.HashedrekordV001Schema(
                 signature=sigstore_rekor_types.Signature1(
                     content=b64_artifact_signature,
-                    publicKey=sigstore_rekor_types.PublicKey(content=b64_cert.decode()),
+                    publicKey=sigstore_rekor_types.PublicKey1(
+                        content=b64_cert.decode()
+                    ),
                 ),
                 data=sigstore_rekor_types.Data(
                     hash=sigstore_rekor_types.Hash(
