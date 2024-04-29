8a72ac425457d21bfe22bc67ad9facd3b461fae3
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2024-04-29 23:56:44 +0200

    test: add test for roundtrip LogEntry conversion
    Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

--- test/unit/test_models.py
+++ test/unit/test_models.py
@@ -34,6 +34,16 @@ class TestLogEntry:
                 inclusion_promise=None,
             )
 
+    def test_logentry_roundtrip(self, signing_bundle):
+        _, bundle = signing_bundle("bundle.txt")
+
+        assert (
+            LogEntry._from_dict_rekor(
+                bundle.log_entry._to_dict_rekor(is_message_signature=True)
+            )
+            == bundle.log_entry
+        )
+
 
 class TestLogInclusionProof:
     def test_valid(self):
