376361a35a167447783a183cc54eaf44f52a91f5
Ramon Petgrave <ramon.petgrave64@gmail.com>
2025-05-20 16:18:13 +0000

    add RekorV2Client and types
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

---
+++ sigstore/_internal/rekor/client_v2.py
@@ -0,0 +1,156 @@
+# Copyright 2025 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Client implementation for interacting with RekorV2.
+"""
+
+from __future__ import annotations
+
+import json
+import logging
+import requests
+from cryptography.hazmat.primitives import serialization
+from cryptography.x509 import Certificate
+import rekor_types
+
+
+
+from sigstore._internal import USER_AGENT
+from sigstore.hashes import Hashed
+from sigstore.models import LogEntry
+
+from sigstore._internal.rekor.v2_types.dev.sigstore.common.v1 import PublicKeyDetails
+from sigstore._internal.rekor.v2_types.dev.sigstore.rekor import v2
+
+_logger = logging.getLogger(__name__)
+
+DEFAULT_REKOR_URL = "https://rekor.sigstore.dev"
+STAGING_REKOR_URL = "https://rekor.sigstage.dev"
+
+DEFAULT_KEY_DETAILS = PublicKeyDetails.PKIX_ECDSA_P384_SHA_256
+
+
+class RekorV2Client:
+    """The internal Rekor client for the v2 API"""
+
+    # TODO: implement get_tile, get_entry_bundle, get_checkpoint.
+
+    def __init__(self, base_url: str) -> None:
+        """
+        Create a new `RekorV2Client` from the given URL.
+        """
+        self.url = f"{base_url}/api/v2"
+        self.session = requests.Session()
+        self.session.headers.update(
+            {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "User-Agent": USER_AGENT,
+            }
+        )
+
+    def __del__(self) -> None:
+        """
+        Terminates the underlying network session.
+        """
+        self.session.close()
+
+    def create_entry(self, request: v2.CreateEntryRequest) -> LogEntry:
+        """
+        Submit a new entry for inclusion in the Rekor log.
+        """
+        # There may be a bug in betterproto, where the V_0_0_2 is changed to V002.
+        # See https://github.com/sigstore/rekor-tiles/blob/bd5893730de581629a5f475923c663f776793496/api/proto/rekor_service.proto#L66.
+        payload = request.to_dict()
+        if "hashedRekordRequestV002" in payload:
+            payload["hashedRekordRequestV0_0_2"] = payload.pop(
+                "hashedRekordRequestV002"
+            )
+        if "dsseRequestV002" in payload:
+            payload["dsseRequestV0_0_2"] = payload.pop("dsseRequestV002")
+        _logger.debug(f"request: {json.dumps(payload)}")
+        resp = self.session.post(f"{self.url}/log/entries", json=payload)
+
+        try:
+            resp.raise_for_status()
+        except requests.HTTPError as http_error:
+            raise RekorClientError(http_error)
+
+        integrated_entry = resp.json()
+        _logger.debug(f"integrated: {integrated_entry}")
+        return LogEntry._from_dict_rekor(integrated_entry)
+
+    @classmethod
+    def _build_create_entry_request(
+        cls,
+        hashed_input: Hashed,
+        signature: bytes,
+        certificate: Certificate,
+    ) -> v2.CreateEntryRequest:
+        return v2.CreateEntryRequest(
+            hashed_rekord_request_v0_0_2=v2.HashedRekordRequestV002(
+                digest=hashed_input.digest,
+                signature=v2.Signature(
+                    content=signature,
+                    verifier=v2.Verifier(
+                        public_key=v2.PublicKey(
+                            raw_bytes=certificate.public_key().public_bytes(
+                                encoding=serialization.Encoding.DER,
+                                format=serialization.PublicFormat.SubjectPublicKeyInfo,
+                            )
+                        ),
+                        key_details=DEFAULT_KEY_DETAILS,
+                    ),
+                ),
+            )
+        )
+
+    @classmethod
+    def production(cls) -> RekorV2Client:
+        """
+        Returns a `RekorV2Client` populated with the default Rekor production instance.
+        """
+        return cls(
+            DEFAULT_REKOR_URL,
+        )
+
+    @classmethod
+    def staging(cls) -> RekorV2Client:
+        """
+        Returns a `RekorV2Client` populated with the default Rekor staging instance.
+        """
+        return cls(STAGING_REKOR_URL)
+
+
+class RekorClientError(Exception):
+    """
+    A generic error in the Rekor client.
+    """
+
+    def __init__(self, http_error: requests.HTTPError):
+        """
+        Create a new `RekorClientError` from the given `requests.HTTPError`.
+        """
+        if http_error.response is not None:
+            try:
+                error = rekor_types.Error.model_validate_json(
+                    http_error.response.text)
+                super().__init__(f"{error.code}: {error.message}")
+            except Exception:
+                super().__init__(
+                    f"Rekor returned an unknown error with HTTP {http_error.response.status_code}"
+                )
+        else:
+            super().__init__(f"Unexpected Rekor error: {http_error}")
---
+++ sigstore/_internal/rekor/v2_types/README.md
@@ -0,0 +1,7 @@
+# V2 Types
+
+These are types meant to be used with RekorV2.
+
+Generated from running `make python` in sigstore/rekor-tiles to generate (although not checked into git) and copied into here.
+
+Eventually, we will move these types into sigstore/protobuf-specs.
