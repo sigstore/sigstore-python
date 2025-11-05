66ed146adf7942da99c7e2d2b2051876e09c3faa
Dustin Ingram <di@users.noreply.github.com>
2025-11-05 20:17:24 +0000

    Update test to use supported public key algorithms

--- test/unit/internal/test_key_details.py
+++ test/unit/internal/test_key_details.py
@@ -15,24 +15,17 @@
 from unittest.mock import Mock
 
 import pytest
-from cryptography.hazmat.primitives.asymmetric import dsa, ec, ed25519, padding, rsa
+from cryptography.hazmat.primitives.asymmetric import ec, ed25519, padding, rsa
 from sigstore_models.common.v1 import PublicKeyDetails
 
 from sigstore._internal.key_details import _get_key_details
 
 
+# The algorithms tested below are from https://github.com/sigstore/fulcio/blob/4a86d8bf45972b58051ba44d91cd96664cf74711/cmd/app/serve.go#L125-L133
 @pytest.mark.parametrize(
     "mock_certificate",
     [
         # ec
-        pytest.param(
-            Mock(
-                public_key=Mock(
-                    return_value=ec.generate_private_key(ec.SECP192R1()).public_key()
-                )
-            ),
-            marks=[pytest.mark.xfail(strict=True)],
-        ),
         Mock(
             public_key=Mock(
                 return_value=ec.generate_private_key(ec.SECP256R1()).public_key()
@@ -49,16 +42,13 @@ from sigstore._internal.key_details import _get_key_details
             )
         ),
         # rsa pkcs1
-        pytest.param(
-            Mock(
-                public_key=Mock(
-                    return_value=rsa.generate_private_key(
-                        public_exponent=65537, key_size=2048
-                    ).public_key()
-                ),
-                signature_algorithm_parameters=padding.PKCS1v15(),
+        Mock(
+            public_key=Mock(
+                return_value=rsa.generate_private_key(
+                    public_exponent=65537, key_size=2048
+                ).public_key()
             ),
-            marks=[pytest.mark.xfail(strict=True)],
+            signature_algorithm_parameters=padding.PKCS1v15(),
         ),
         Mock(
             public_key=Mock(
@@ -75,34 +65,6 @@ from sigstore._internal.key_details import _get_key_details
                 ).public_key()
             ),
             signature_algorithm_parameters=padding.PKCS1v15(),
-        ),
-        # rsa pss
-        pytest.param(
-            Mock(
-                public_key=Mock(
-                    return_value=rsa.generate_private_key(
-                        public_exponent=65537, key_size=2048
-                    ).public_key()
-                ),
-                signature_algorithm_parameters=padding.PSS(None, 0),
-            ),
-            marks=[pytest.mark.xfail(strict=True)],
-        ),
-        Mock(
-            public_key=Mock(
-                return_value=rsa.generate_private_key(
-                    public_exponent=65537, key_size=3072
-                ).public_key()
-            ),
-            signature_algorithm_parameters=padding.PSS(None, 0),
-        ),
-        Mock(
-            public_key=Mock(
-                return_value=rsa.generate_private_key(
-                    public_exponent=65537, key_size=4096
-                ).public_key()
-            ),
-            signature_algorithm_parameters=padding.PSS(None, 0),
         ),
         # ed25519
         Mock(
@@ -110,16 +72,6 @@ from sigstore._internal.key_details import _get_key_details
                 return_value=ed25519.Ed25519PrivateKey.generate().public_key(),
                 signature_algorithm_parameters=None,
             )
-        ),
-        # unsupported
-        pytest.param(
-            Mock(
-                public_key=Mock(
-                    return_value=dsa.generate_private_key(key_size=1024).public_key()
-                ),
-                signature_algorithm_parameters=None,
-            ),
-            marks=[pytest.mark.xfail(strict=True)],
         ),
     ],
 )
