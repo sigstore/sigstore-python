c7c80decdd19ed98c8523ad1604c3714119a5ddd
Colleen Murphy <cmurphy@users.noreply.github.com>
2026-01-15 19:23:29 -0800

    Include SCT extension in signature data (#1659)

--- sigstore/_internal/sct.py
+++ sigstore/_internal/sct.py
@@ -110,7 +110,7 @@ def _pack_digitally_signed(
     # Assemble a format string with the certificate length baked in and then pack the digitally
     # signed data
     # fmt: off
-    pattern = f"!BBQH{len(signed_entry)}sH"
+    pattern = f"!BBQH{len(signed_entry)}sH{len(sct.extension_bytes)}s"
     timestamp = sct.timestamp.replace(tzinfo=timezone.utc)
     data = struct.pack(
         pattern,
@@ -120,6 +120,7 @@ def _pack_digitally_signed(
         sct.entry_type.value,               # entry_type (x509_entry(0) | precert_entry(1))
         signed_entry,                       # select(entry_type) -> signed_entry (see above)
         len(sct.extension_bytes),           # extensions (opaque CtExtensions<0..2^16-1>)
+        sct.extension_bytes,
     )
     # fmt: on
 
